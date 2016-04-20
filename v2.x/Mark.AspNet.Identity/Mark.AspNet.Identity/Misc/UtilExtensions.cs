edc18320c5f823e274ea6d917ada2e3a7022d9c7
mahbub-alam <mahbub002@ymail.com>
2016-04-20 21:09:15 +0600

    Added DBNull support extensions for null value in core ASP.NET Identity module

---
+++ v2.x/Mark.AspNet.Identity/Mark.AspNet.Identity/Misc/UtilExtensions.cs
@@ -0,0 +1,61 @@
+﻿// Written by: MAB
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mark.Core
+{
+    /// <summary>
+    /// Represents general utility extensions.
+    /// </summary>
+    public static class UtilExtensions
+    {
+        /// <summary>
+        /// Get DB null if the value is null; otherwise, get the actual value.
+        /// </summary>
+        /// <param name="target">Target object.</param>
+        /// <returns>Returns DB null if null; otherwise, returns actual value.</returns>
+        public static object GetDBNullIfNull(this string target)
+        {
+            if (target == null)
+            {
+                return DBNull.Value;
+            }
+
+            return target;
+        }
+
+        /// <summary>
+        /// Get DB null if the value is null; otherwise, get the actual value.
+        /// </summary>
+        /// <param name="target">Target object.</param>
+        /// <returns>Returns DB null if null; otherwise, returns actual value.</returns>
+        public static object GetDBNullIfNull(this DateTime? target)
+        {
+            if (target == null || target == DateTime.MinValue)
+            {
+                return DBNull.Value;
+            }
+
+            return target.Value;
+        }
+
+        /// <summary>
+        /// Get DB null if the value is null; otherwise, get the actual value.
+        /// </summary>
+        /// <param name="target">Target object.</param>
+        /// <returns>Returns DB null if null; otherwise, returns actual value.</returns>
+        public static object GetDBNullIfNull<T>(this T? target) where T : struct
+        {
+            if (target == null)
+            {
+                return DBNull.Value;
+            }
+
+            return target.Value;
+        }
+    }
+}
