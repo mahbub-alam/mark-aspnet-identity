74e85b9ad1e568bd09eb5378b2c73817bed447c8
mahbub-alam <mahbub002@ymail.com>
2016-12-01 21:31:56 +0600

    Changed email address

--- v2.x/src/Mark.AspNet.Identity.EntityFramework/Misc/IdentityDbContext.cs
+++ v2.x/src/Mark.AspNet.Identity.EntityFramework/Misc/IdentityDbContext.cs
@@ -1,5 +1,5 @@
 ﻿//
-// Copyright 2016, Mahbub Alam (mahbub002@ymail.com)
+// Copyright 2016, Mahbub Alam (mahbub002@gmail.com)
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
